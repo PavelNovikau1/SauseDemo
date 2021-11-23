383230ccfcb7585ebf1b7e8c2bc1d41dfc206836
Pavel <razor8ne8@icloud.com>
2021-11-23 20:41:21 +0200

    Add config.yml

--- src/test/java/tests/CheckOutCompleteTests.java
+++ src/test/java/tests/CheckOutCompleteTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import io.qameta.allure.Description;
 import org.testng.Assert;
+import org.testng.ReporterConfig;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class CheckOutCompleteTests extends CheckOutStepTwoTests {
@@ -10,7 +12,7 @@ public class CheckOutCompleteTests extends CheckOutStepTwoTests {
     @Description(value = "product purchase verification test")
     public  void verificationOfSuccessfulPurchaseTest() {
         loginPage.openPage()
-                 .login(System.getProperty("username"), System.getProperty("password"))
+                 .login("standard_user", "secret_sauce" )
                  .addProductToCart("Sauce Labs Bolt T-Shirt");
         cartPage.openCardPage()
                  .checkOutButtonClick()
